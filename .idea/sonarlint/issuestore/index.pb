2cfddccffa482a99c6dfc5fb639fb686d1845896
layrodrgs <layrodrgs@gmail.com>
2020-10-09 09:15:08 -0300

    Recursos uteis do Mockito

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -9,3 +9,9 @@ U
 .src/main/java/org/example/CadastroPessoas.java,9\f\9f165b3673a2980d7e8a5f09a6c45c91226b779b
 b
 2src/test/java/org/example/CadastroPessoasTest.java,6\0\606a7604143e5c0ad95c06f6e20ba30adfdb7c2f
+e
+5src/main/java/org/example/CadastroVazioException.java,7\d\7d3c02292224db4269579e4d6b0c5380fe5dc057
+e
+5src/main/java/org/example/PessoaSemNomeException.java,4\2\42f71fc0f5f820797026e1d8d3b2cbafc1212764
+[
++src/test/java/org/example/MockitoTests.java,2\5\2544939318298073a0bf8ea0babf68b7a5306f45
---
+++ src/test/java/org/example/MockitoTests.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MockitoTests {
+
+    @Mock
+    List<String> lista;
+
+    @Test
+    public void primeiroTesteMockito(){
+        lista.size();
+        lista.add("");
+
+       InOrder inOrder = Mockito.inOrder(lista);
+        inOrder.verify(lista).size();
+        inOrder.verify(lista).add("");
+
+    }
+}
